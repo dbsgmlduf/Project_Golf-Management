7a5771de4a3b19fae1b7aaa6562771bc0d1bb85c
hemingwayInTheX <leeht620@gmail.com>
2021-09-27 20:29:46 +0900

    Add passport to authenticate user

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { User } = require('../models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 router.post('/', async(req, res) => {
     const { usertype, username, email, password } = req.body;
@@ -16,26 +17,42 @@ router.post('/', async(req, res) => {
         res.json("SUCCESS!!");
     });
 });
-//res 여러개 = ERR_HTTP_HEADERS_SENT
-router.post('/login', async(req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({where: {email: email}});
-    if(!user){
-        res.json({error:"USER DOSEN'T EXIST!"});
-    }
-    bcrypt.compare(password, user.password).then((match) => {
-        if(!match){
-            //return res.json({error: "WRONG USERNAME & PASSWORD!"});
-        }
-        //return res.json("YOU LOGGED IN!!");
-    });
 
-    const accessToken = jwt.sign(
-        { email: user.email },
-        process.env.JWT_ACCESS_TOKEN_SECRET
-    );
-    
-    res.json({message: "FUCKING TOKEN!", token: accessToken});
+router.post('/login', passport.authenticate('local', {session: false}),
+        async (req, res, error) => {
+            const user = req.body
+            const accessToken = 
+                jwt.sign(
+                    { email: user.email },
+                    process.env.JWT_ACCESS_TOKEN_SECRET
+                );
+            res.json({
+                message: "LOGIN SUCCESS!",
+                accessToken
+            });
 });
+
+//res 여러개 = ERR_HTTP_HEADERS_SENT
+// router.post('/login', async(req, res) => {
+//     const {email, password} = req.body;
+//     const user = await User.findOne({where: {email: email}});
+//     if(!user){
+//         res.json({error:"USER DOSEN'T EXIST!"});
+//     }
+//     bcrypt.compare(password, user.password).then((match) => {
+//         if(!match){
+//             //return res.json({error: "WRONG USERNAME & PASSWORD!"});
+//         }
+//         //return res.json("YOU LOGGED IN!!");
+//     });
+
+//     const accessToken = jwt.sign(
+//         { email: user.email },
+//         process.env.JWT_ACCESS_TOKEN_SECRET
+//     );
+    
+//     res.json({message: "FUCKING TOKEN!", token: accessToken});
+// });
+
 
 module.exports = router;
