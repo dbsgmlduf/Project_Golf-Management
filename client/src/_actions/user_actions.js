5cdf47cf8ffa50bba5fb1aac71b96796f7082572
hemingwayInTheX <leeht620@gmail.com>
2021-10-10 22:20:46 +0900

    Correct axios param

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -9,7 +9,7 @@ export function loginUser(data){
 }
 
 export function register(data){
-    const req = axios.post('http://localhost:7000/auth', data)
+    const req = axios.post('http://localhost:7000/auth/userReg', data)
     .then(res=>res.data);
     return{type :REGISTER, payload:req}
 }
