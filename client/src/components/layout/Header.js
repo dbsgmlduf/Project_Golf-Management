4b9ddd7c54c6c085b5631eab685ed3253663e57f
dbsgmlduf <huiyy9211@gmail.com>
2021-10-22 11:18:17 +0900

    header appbar

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -3,27 +3,54 @@ import LoginButton from '../UI/atoms/login_button/index';
 import LogoutButton from '../UI/atoms/logout_button/index';
 import RegisterButton from '../UI/atoms/register_button/index';
 import isLogin from '../../lib/isLogin';
+import { AppBar, Box, IconButton,makeStyles,Toolbar, Typography} from '@material-ui/core';
+import MenuIcon from "@material-ui/icons/Menu";
 
+const useeStyles = makeStyles((theme)=>({
+    appbar:{
+        background:'none',
+    },
+    title:{
+        flexGrow:'1',
+        color :'#000000',
+        fontSize :'1rem',
+    },
+    icon:{
+        color :'#000000',
+        fontSize :'2rem',
+    },
+}));
 
 const Header = () => {
+    const classes = useeStyles();
     if(isLogin()){
         return(
-            <div className="page-header">
-                <h1>GOLFTAK</h1>
-                <div className="navbar_item">
-                    <LogoutButton/>
-                </div>
-            </div>
+            
+            <Box sx={{flexGrow:1}}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton><menuIcon/></IconButton>
+                        <Typography>GOLFTAK</Typography>
+                        <LogoutButton/>
+                    </Toolbar>
+                </AppBar>
+            </Box>
             )
     }
     else{
         return(
-            <div className="page-header">
-                <h1>GOLFTAK</h1>
-                <div className="navbar_item">
-                    <LoginButton/><RegisterButton/>
-                </div>
-            </div>
+           <div>
+                <AppBar className={classes.appbar} elevation={0}>
+                    <Toolbar>
+                        <IconButton><MenuIcon className={classes.icon}/></IconButton>
+                        <Typography className={classes.title}>
+                            <h1>GOLFTAK</h1>
+                        </Typography>
+                        <LoginButton /><RegisterButton/>
+                    </Toolbar>
+                </AppBar>
+            </div> 
+            
     );
 }
 };
