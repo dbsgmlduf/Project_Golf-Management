bfaa623350ae73d66551631615c22fa451290f26
hemingwayInTheX <leeht620@gmail.com>
2021-10-26 15:29:25 +0900

    clear

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -2,6 +2,14 @@
 import axios from "axios";
 import { LOGIN,REGISTER } from "./types";
 
+axios.interceptors.request.use((config) => {
+    config.headers = {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+    };
+    config.timeout = 2000;
+    return config;
+});
+
 export function loginUser(data){
     const req = axios.post('http://localhost:7000/api/users/login', data)
     .then(res=>res.data);
