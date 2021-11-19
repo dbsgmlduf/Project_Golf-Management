1d68aac35fb110cb3ce89e46da1c610019a6332a
hemingwayInTheX <leeht620@gmail.com>
2021-11-19 23:32:54 +0900

    Correct Index

--- server/routes/instructor/index.js
+++ server/routes/instructor/index.js
@@ -7,17 +7,10 @@ router.patch('/accept', middlewares.jwtAuth, controller.setAccept);
 router.get('/mylearner', middlewares.jwtAuth, controller.getMylist);
 router.get('/mylist', middlewares.jwtAuth, controller.getList);
 router.post('/classinfo', middlewares.jwtAuth, controller.inputInfo);
-
-//===수정1===
 router.get('/getinfo/:username/:instructor', middlewares.jwtAuth, controller.getInfo);
-//===========
-
 router.get('/detailinfo/:username/:session_no', middlewares.jwtAuth, controller.getDetailInfo);
 router.patch('/:username/:session_no', middlewares.jwtAuth, controller.updateClassInfo);
-
-//===추가1,2===
 router.post('/dateinfo', middlewares.jwtAuth, controller.inputDateInfo);
 router.get('/:username', middlewares.jwtAuth, controller.getCertainLecturer);
-//============
 
 module.exports = router;
