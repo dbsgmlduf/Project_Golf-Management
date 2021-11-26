e5ed4252ef14c58c60256d736d6c1b59b4927c82
dbsgmlduf <huiyy9211@gmail.com>
2021-11-26 13:22:12 +0900

    learner app page table change

--- client/src/components/UI/atoms/lecturer/index.js
+++ client/src/components/UI/atoms/lecturer/index.js
@@ -1,18 +1,52 @@
-import { TableCell, TableRow } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    AccordionSummary,
+    AccordionDetails,
+    Accordion,
+    Typography,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import useStyles from './style';
 import SelectLecturer from '../lecturer_select';
 const Lecturers = (props) => {
+    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
     return (
-        <TableRow>
-            <TableCell align="center">{props.username}</TableCell>
-            <TableCell align="center">
-                <SelectLecturer
-                    username={props.username}
-                    id={props.id}
-                    enrollData={props.enrollData}
-                />
-            </TableCell>
-        </TableRow>
+        <>
+            <Accordion
+                expanded={expanded === 'panel1'}
+                onChange={handleChange('panel1')}
+            >
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                >
+                    <Typography
+                        sx={{ width: '100%', flexShrink: 1 }}
+                        className={classes.username}
+                    >
+                        {props.username}
+                    </Typography>
+
+                    <SelectLecturer
+                        username={props.username}
+                        id={props.id}
+                        enrollData={props.enrollData}
+                    />
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Nulla facilisi. Phasellus sollicitudin nulla et quam
+                        mattis feugiat. Aliquam eget maximus est, id dignissim
+                        quam.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </>
     );
 };
 
--- client/src/components/UI/atoms/lecturer_list/index.js
+++ client/src/components/UI/atoms/lecturer_list/index.js
@@ -1,14 +1,6 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import {
-    Grid,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-} from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Grid } from '@material-ui/core';
+
 import useStyles from './style';
 import Lecturers from '../lecturer';
 import LecturerSearchBar from '../lecturer_search';
@@ -50,47 +42,51 @@ const LecturerList = () => {
     const [enrollYesData, setEnrollYesData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    const fetchUsers = useCallback(async () => {
-        try {
-            // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-            setError(null);
-            setUsers(null);
-            // loading 상태를 true 로 바꿉니다.
-            setLoading(true);
-            const responseAll = await axios.get('/api/learners/list'); //전체 강사list api
-            const responseEnroll = await axios.get('api/learners/');
-            for (let i = 0; i < responseAll.data.list.length; i++) {
-                for (let j = 0; j < responseEnroll.data.status.length; j++) {
-                    if (
-                        responseAll.data.list[i].id ===
-                            responseEnroll.data.status[j].lecturer.id &&
-                        responseEnroll.data.status[j].isenrolled
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+                setError(null);
+                setUsers(null);
+                // loading 상태를 true 로 바꿉니다.
+                setLoading(true);
+                const responseAll = await axios.get('/api/learners/list'); //전체 강사list api
+                const responseEnroll = await axios.get('api/learners/');
+                for (let i = 0; i < responseAll.data.list.length; i++) {
+                    for (
+                        let j = 0;
+                        j < responseEnroll.data.status.length;
+                        j++
                     ) {
-                        setEnrollYesData((prevList) => [
-                            ...prevList,
-                            responseAll.data.list[i],
-                        ]);
-                    } else if (
-                        responseAll.data.list[i].id ===
-                            responseEnroll.data.status[j].lecturer.id &&
-                        !responseEnroll.data.status[j].isenrolled
-                    ) {
-                        setEnrollData((prevList) => [
-                            ...prevList,
-                            responseAll.data.list[i],
-                        ]);
+                        if (
+                            responseAll.data.list[i].id ===
+                                responseEnroll.data.status[j].lecturer.id &&
+                            responseEnroll.data.status[j].isenrolled
+                        ) {
+                            setEnrollYesData((prevList) => [
+                                ...prevList,
+                                responseAll.data.list[i],
+                            ]);
+                        } else if (
+                            responseAll.data.list[i].id ===
+                                responseEnroll.data.status[j].lecturer.id &&
+                            !responseEnroll.data.status[j].isenrolled
+                        ) {
+                            setEnrollData((prevList) => [
+                                ...prevList,
+                                responseAll.data.list[i],
+                            ]);
+                        }
                     }
                 }
+                setUsers(responseAll.data.list);
+            } catch (e) {
+                setError(e);
             }
-            setUsers(responseAll.data.list);
-        } catch (e) {
-            setError(e);
-        }
-        setLoading(false);
-    }, []);
-    useEffect(() => {
+            setLoading(false);
+        };
         fetchUsers();
-    }, [fetchUsers]);
+    }, []);
 
     if (loading) return <div>로딩중..</div>;
     if (error) return <div>에러가 발생했습니다</div>;
@@ -102,45 +98,23 @@ const LecturerList = () => {
                 value={serchKeyword}
                 handleSeachKey={handleSeachKey}
             />
-            <TableContainer component={Paper} className={classes.paper}>
-                <Table
-                    aria-label="lecturer list"
-                    className={classes.table}
-                    size="small"
-                    sx={{ minWidth: 650 }}
-                    stickyHeader
-                >
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">강사 이름</TableCell>
-                            <TableCell align="center">강사 추가</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {serchKeyword
-                            ? filteredData(users)
-                            : users.map((c) => {
-                                  for (
-                                      let i = 0;
-                                      i < enrollYesData.length;
-                                      i++
-                                  ) {
-                                      if (c.id === enrollYesData[i].id) {
-                                          return null;
-                                      }
-                                  }
-                                  return (
-                                      <Lecturers
-                                          key={c.username}
-                                          username={c.username}
-                                          id={c.id}
-                                          enrollData={enrollData}
-                                      />
-                                  );
-                              })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            {serchKeyword
+                ? filteredData(users)
+                : users.map((c) => {
+                      for (let i = 0; i < enrollYesData.length; i++) {
+                          if (c.id === enrollYesData[i].id) {
+                              return null;
+                          }
+                      }
+                      return (
+                          <Lecturers
+                              key={c.username}
+                              username={c.username}
+                              id={c.id}
+                              enrollData={enrollData}
+                          />
+                      );
+                  })}
         </Grid>
     );
 };
