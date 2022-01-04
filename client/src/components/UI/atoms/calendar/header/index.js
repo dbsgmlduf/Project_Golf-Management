16d44197a81e144b2755b1d05c60e0c72fd5d04a
dbsgmlduf <huiyy9211@gmail.com>
2022-01-04 19:06:12 +0900

    fail make

---
+++ client/src/components/UI/atoms/calendar/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+const Title = () => {
+    return (
+        <>
+            <ArrowLeftIcon />
+            <span>month year</span>
+            <ArrowRightIcon />
+        </>
+    );
+};
+
+export default Title;
--- client/src/components/UI/modules/calendarMain/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import useStyles from './style';
-import { Card, useMediaQuery } from '@material-ui/core';
-const Main = () => {
-    const classes = useStyles();
-    return <Card></Card>;
-};
-
-export default Main;
