4a0667758390f79d85aec47dceb601a2022ada89
dbsgmlduf <huiyy9211@gmail.com>
2022-01-07 22:37:22 +0900

    react big calendar set

--- client/src/components/page/CalendarPage.js
+++ client/src/components/page/CalendarPage.js
@@ -2,12 +2,11 @@ import Header from '../UI/modules/header/Header';
 import Footer from '../UI/modules/footer/Footer';
 import { Grid } from '@mui/material';
 //import Main from '../UI/modules/calendarMain';
-import BigCalendar from 'react-big-calendar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
 
-moment.locale('en-GB');
-BigCalendar.momentLocalizer(moment);
+const localizer = momentLocalizer(moment);
 
 const CalendarPage = (props) => {
     return (
@@ -15,14 +14,15 @@ const CalendarPage = (props) => {
             <Header />
             {/* <Main /> */}
             <div style={{ height: 500 }}>
-                <BigCalendar
-                    events={[]}
+                <Calendar
+                    //events={[]}
                     step={60}
                     view="month"
                     views={['month']}
                     min={new Date(2022, 0, 1, 9, 0)}
                     max={new Date(2022, 0, 1, 10, 0)}
-                    date={new Date()}
+                    defaultDate={new Date()}
+                    localizer={localizer}
                 />
             </div>
             <Footer />
